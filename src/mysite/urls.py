c0b1134408d726d00e60896f688f7d44d1965057
dokuta <drstraaange@gmail.com>
2020-12-06 02:36:54 +0900

    Tutorial 1: Serialization

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -23,15 +23,17 @@ urlpatterns = [
 """
 from django.urls import include, path
 from rest_framework import routers
-from mysite.quickstart import views
+from mysite.quickstart.views import UserViewSet, GroupViewSet
+from snippets.views import snippet_list, snippet_detail
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
